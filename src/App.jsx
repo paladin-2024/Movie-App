b5e5d47bc1742153236b8a28c35cf01a9162b095
paladin-2024 <cnzabb@gmail.com>
2025-07-14 02:22:11 +0300

    add like functionality and click counter to Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Card = ({title}) => {
+    const [count, setCount] = useState(0)
     const [hasLiked, setHasLiked] = useState(false)
+
+    useEffect(()=>{
+        console.log(`${title} has been liked: ${hasLiked}`)
+    }, [hasLiked]);
+
+
+
     return (
-        <div className="card">
-            <h2>{title} </h2>
+        <div className="card" onClick={()=>setCount(count+1)}>
+            <h2>{title}<br/>{count || null} </h2>
             <button onClick={()=> setHasLiked(!hasLiked)}>
                 {hasLiked ? '❤️' : '🤍'}
             </button>
