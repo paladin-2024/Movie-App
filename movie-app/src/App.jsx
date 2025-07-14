674862f3fb6d739abe66a01dafa505158c78b043
paladin-2024 <cnzabb@gmail.com>
2025-07-14 17:38:26 +0300

    add MovieCard component to display movie details in App

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react'
 import Search from "./components/Search.jsx";
 import Spinner from "./components/spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -70,7 +71,7 @@ const App = () => {
                     ) : (
                         <ul>
                             {movieList.map((movie) => (
-                                <p key={movie.id} className="text-white">{movie.title}</p>
+                                <MovieCard key={movie.id} movie={movie} />
                             ))}
                         </ul>
                     ) }
